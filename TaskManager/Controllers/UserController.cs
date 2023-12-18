e42d6612d86da7c1d0013553bf29ce3ed0cea136
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2023-12-18 20:09:48 +0300

    Create view page for registration in system!

--- TaskManager/Controllers/UserController.cs
+++ TaskManager/Controllers/UserController.cs
@@ -5,6 +5,10 @@ namespace TaskManager.Controllers
     [Route("[controller]")]
     public class UserController : Controller
     {
+        /// <summary>
+        /// return html css page for enter in system for get request
+        /// </summary>
+        /// <returns>return html css page for enter</returns>
         [HttpGet("[action]")]
         [HttpGet("/")]
         public IActionResult Enter()
@@ -12,6 +16,10 @@ namespace TaskManager.Controllers
             return View();
         }
 
+        /// <summary>
+        /// return html css page for registration in system for get request
+        /// </summary>
+        /// <returns>return html css page for registration</returns>
         [HttpGet("[action]")]
         public IActionResult Registration()
         {
