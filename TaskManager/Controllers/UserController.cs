065abb6afa109120b8184cb498c08bbee9f31c98
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2023-12-18 19:17:31 +0300

    Create View page for login in system!

---
+++ TaskManager/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManager.Controllers
+{
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        [HttpGet("[action]")]
+        [HttpGet("/")]
+        public IActionResult Enter()
+        {
+            return View();
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
