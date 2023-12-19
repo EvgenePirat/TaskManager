5d29e3782e45f5f52c5a8466abc89e2aea65a305
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2023-12-19 18:58:59 +0300

    add link one to many between user and role

--- DataAccessLayer/ApplicationDbContext.cs
+++ DataAccessLayer/ApplicationDbContext.cs
@@ -8,11 +8,26 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer
 {
+    /// <summary>
+    /// Class for registration our models in context application,setting their and change setting connection to BD 
+    /// </summary>
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
+
+        /// <summary>
+        /// Method from Fluent API give possibility setting models and link between models
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Role)
+                .WithMany(u => u.Users)
+                .HasForeignKey(u => u.RoleId);
+        }
     }
 }
