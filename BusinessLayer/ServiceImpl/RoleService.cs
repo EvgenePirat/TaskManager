990a367ec6fab9573d0a780da98c804086e98599
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2023-12-19 21:50:21 +0300

    release logic service for get all roles!

--- BusinessLayer/ServiceImpl/RoleService.cs
+++ BusinessLayer/ServiceImpl/RoleService.cs
@@ -1,5 +1,7 @@
 ﻿using BusinessLayer.DTO.Response;
+using BusinessLayer.Mapper;
 using BusinessLayer.ServiceContract;
+using DataAccessLayer.RepositoryContract;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,9 +15,20 @@ namespace BusinessLayer.ServiceImpl
     /// </summary>
     public class RoleService : IRoleService
     {
-        public Task<List<RoleResponse>> GetAllRoles()
+        private readonly IRoleRepository _roleRepository;
+
+        public RoleService(IRoleRepository roleRepository)
         {
-            throw new NotImplementedException();
+            _roleRepository = roleRepository;
+        }
+
+        /// <summary>
+        /// method for return all roles
+        /// </summary>
+        /// <returns>returned all roles</returns>
+        public async Task<List<RoleResponse>> GetAllRoles()
+        {
+            return _roleRepository.GetAllRoles().Select(role => RoleMapper.RoleToRoleResponse(role)).ToList();
         }
     }
 }
