2f7aa30afd88be3d35245b7accade055b33fdb60
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2023-12-19 21:02:35 +0300

    create migration with added new roles in bd

---
+++ DataAccessLayer/Migrations/20231219180033_ChangeDateForMigratyion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class AddedNewDataForRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { new Guid("77faa92e-48cc-45f0-aafb-7c4031839329"), "Admin" });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { new Guid("fabab501-8b10-4f1f-987e-210e59cd7a2b"), "User" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("77faa92e-48cc-45f0-aafb-7c4031839329"));
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("fabab501-8b10-4f1f-987e-210e59cd7a2b"));
+        }
+    }
+}
