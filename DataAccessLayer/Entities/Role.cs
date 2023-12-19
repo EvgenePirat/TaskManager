fb25f67d53a4288fdfcf8a945824dbc23486d334
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2023-12-19 18:11:41 +0300

    create model role in data access layer

---
+++ DataAccessLayer/Entities/Role.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entities
+{
+    public class Role
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [StringLength(20)]
+        public string? Name { get; set; }
+    }
+}
