37fe42ebcb659fb64985512f4adfcf90cc657505
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2023-12-19 21:12:43 +0300

    I created repository and impl for get all roles from db!

---
+++ DataAccessLayer/RepositoryContract/IRoleRepository.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.RepositoryContract
+{
+    /// <summary>
+    /// Perpesents data access logic for managing Role entity
+    /// </summary>
+    public interface IRoleRepository
+    {
+        /// <summary>
+        /// Method for gets all roles from basedata
+        /// </summary>
+        /// <returns>returned list with role from db</returns>
+        public List<Role> GetAllRoles();
+    }
+}
---
+++ DataAccessLayer/RepositoryImpl/RoleRepository.cs
@@ -0,0 +1,25 @@
+﻿using DataAccessLayer.Entities;
+using DataAccessLayer.RepositoryContract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.RepositoryImpl
+{
+    public class RoleRepository : IRoleRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RoleRepository(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public List<Role> GetAllRoles()
+        {
+            return _context.Roles.ToList();
+        }
+    }
+}
