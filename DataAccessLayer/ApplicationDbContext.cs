61e52a4d9012aa73889b2484b5019438fbe7df64
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2023-12-19 18:50:47 +0300

    created dbcontext and set path for db!

---
+++ DataAccessLayer/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+    }
+}
