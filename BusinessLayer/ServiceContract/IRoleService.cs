a9295a7fd643d75c7354b1f92cc7ff1ef25ff30b
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2023-12-19 21:23:45 +0300

    create service logic for role with method get all roles!

---
+++ BusinessLayer/ServiceContract/IRoleService.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.DTO.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ServiceContract
+{
+    /// <summary>
+    /// Rerpesents business logic for manipoulating role entity
+    /// </summary>
+    public interface IRoleService
+    {
+        /// <summary>
+        /// Method for get all roles
+        /// </summary>
+        /// <returns>return a list of objects of roleresponse type</returns>
+        public Task<List<RoleResponse>> GetAllRoles();
+    }
+}
---
+++ BusinessLayer/ServiceImpl/RoleService.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.DTO.Response;
+using BusinessLayer.ServiceContract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ServiceImpl
+{
+    /// <summary>
+    /// Class for implementation methods for Role Service
+    /// </summary>
+    public class RoleService : IRoleService
+    {
+        public Task<List<RoleResponse>> GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
