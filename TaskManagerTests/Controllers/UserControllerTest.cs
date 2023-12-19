dffd5300fff9feb683e634d73a3faf86d3dbda7f
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2023-12-19 19:50:45 +0300

    create two tests for controller user!

---
+++ TaskManagerTests/Controllers/UserControllerTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Controllers;
+
+namespace TaskManagerTests.Controllers
+{
+    public class UserControllerTest
+    {
+        #region Enter
+
+        [Fact(DisplayName = "Test enter controller")]
+        public void Enter_ShouldReturnEnterView()
+        {
+            //arrange
+            UserController userController = new UserController();
+
+            //act
+            IActionResult actionResult = userController.Enter();
+
+            //arrange
+            ViewResult result = Assert.IsType<ViewResult>(actionResult);
+        }
+        #endregion
+
+        #region
+
+        [Fact(DisplayName = "Test registration controller")]
+        public void Registration_ShouldReturnRegistrationView()
+        {
+            //arrange
+            UserController userController = new UserController();
+
+            //act
+            IActionResult actionResult = userController.Registration();
+
+            //arrange
+            ViewResult result = Assert.IsType<ViewResult>(actionResult);
+        }
+
+        #endregion
+    }
+}
