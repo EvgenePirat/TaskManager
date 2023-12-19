4ee75d0fdfbd4e8f9c43f053eb03178b10a13af9
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2023-12-19 18:31:10 +0300

    add comments for role model

--- DataAccessLayer/Entities/Role.cs
+++ DataAccessLayer/Entities/Role.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Entities
 {
+    /// <summary>
+    /// Role domain model class for hold data from user
+    /// </summary>
     public class Role
     {
         [Key]
