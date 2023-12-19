6d5949b1cad40fc753e1ebe509539dd0365e2a94
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2023-12-19 20:22:58 +0300

    create dto for added user and role in system!

---
+++ BusinessLayer/DTO/RoleRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.DTO
+{
+    /// <summary>
+    /// DTO for model user
+    /// </summary>
+    public class RoleRequest
+    {
+        [Required(ErrorMessage="Role name can be")]
+        public string? Name { get; set; }
+    }
+}
