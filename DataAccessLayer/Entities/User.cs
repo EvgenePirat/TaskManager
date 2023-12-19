1ff8738c9e30151383ea91fd131d3c1aebbde3d0
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2023-12-19 18:27:42 +0300

    create model user in data access layour

---
+++ DataAccessLayer/Entities/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entities
+{
+    /// <summary>
+    /// Domain model class for hold user dates in basedata
+    /// </summary>
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        [StringLength(50)]
+        public string? Name { get; set; }
+
+        [StringLength(30)]
+        public string? UserName { get; set; }
+
+        [StringLength(50)]
+        public string? Password { get; set; }
+
+        [StringLength(50)]
+        public string? Email { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public Guid RoleId { get; set; }
+
+        public Role? Role { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime CreateAccount { get; set; }
+    }
+}
