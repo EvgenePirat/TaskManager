6cba42820425c91ddf5141cf1959903758229401
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2023-12-20 11:42:34 +0300

    create task controller with method for get mane page for task

---
+++ TaskManager/Controllers/TaskController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManager.Controllers
+{
+    /// <summary>
+    /// controller for working with task logic
+    /// </summary>
+    [Route("[controller]")]
+    public class TaskController : Controller
+    {
+        /// <summary>
+        /// Method for get home page for manager task
+        /// </summary>
+        /// <returns>returned home page for task logic</returns>
+        [HttpGet("[action]")]
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+    }
+}
