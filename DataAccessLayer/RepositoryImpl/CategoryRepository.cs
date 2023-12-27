558743c203760487733ea481612e27060c123fe5
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2023-12-27 12:59:33 +0300

    add repository for working with categories

---
+++ DataAccessLayer/RepositoryImpl/CategoryRepository.cs
@@ -0,0 +1,34 @@
+﻿using DataAccessLayer.Entities;
+using DataAccessLayer.RepositoryContract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.RepositoryImpl
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public async Task<Category?> AddCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+
+            return await _context.Categories.Include("Tasks").FirstOrDefaultAsync(temp => temp.Id == category.Id);
+        }
+
+        public async Task<List<Category>> GetAllCategories(Guid UserId)
+        {
+            return await _context.Categories.Include("Tasks").Where(temp => temp.UserId == UserId).ToListAsync();
+        }
+    }
+}
--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -16,6 +16,7 @@ namespace TaskManager
             builder.Services.AddScoped<IRoleRepository, RoleRepository>();
             builder.Services.AddScoped<IUserRepository, UserRepository>();
             builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
             builder.Services.AddControllersWithViews();
 
             builder.Services.AddDbContext<ApplicationDbContext>(option =>
