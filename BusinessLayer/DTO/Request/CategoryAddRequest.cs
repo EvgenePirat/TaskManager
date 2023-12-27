c24224bf3ae28c41ba0a1b838c81843a1059b1de
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2023-12-27 12:38:06 +0300

    add dto for process add category!

---
+++ BusinessLayer/DTO/Request/CategoryAddRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.DTO.Request
+{
+    /// <summary>
+    /// DTO for interesting a new category
+    /// </summary>
+    public class CategoryAddRequest
+    {
+        [Required(ErrorMessage = "Category name can't be blank")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "User id can't be blank")]
+        public Guid UserId { get; set; }
+    }
+}
---
+++ BusinessLayer/DTO/Response/CategoryResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.DTO.Response
+{
+    /// <summary>
+    /// DTO for response category entity
+    /// </summary>
+    public class CategoryResponse
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public DateTime? CreatedDate { get; set; };
+    }
+}
