127500fa4990de8777b74f8946276aa81fd33254
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2023-12-27 12:12:36 +0300

    added page for add new category!

---
+++ TaskManager/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManager.Controllers
+{
+    /// <summary>
+    /// Controller with methods for working with category
+    /// </summary>
+    [Route("[controller]")]
+    public class CategoryController : Controller
+    {
+        /// <summary>
+        /// Method for show page for added new category
+        /// </summary>
+        /// <returns>returned page for added new category</returns>
+        [HttpGet("[action]")]
+        public IActionResult AddNewCategory()
+        {
+            return View("AddCategory");
+        }
+    }
+}
