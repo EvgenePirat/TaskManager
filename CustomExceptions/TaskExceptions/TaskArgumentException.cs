989ede4478a2e90d2cfce47dd6a827931cc803bb
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2024-01-06 21:15:34 +0300

    #4 added custom exception for task

---
+++ CustomExceptions/TaskExceptions/TaskArgumentException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExceptions.TaskExceptions
+{
+    public class TaskArgumentException : Exception
+    {
+        public TaskArgumentException() : base()
+        {
+            
+        }
+
+        public TaskArgumentException(string? message) : base(message)
+        {
+            
+        }
+
+        public TaskArgumentException(string? message, Exception? innerException) : base(message, innerException) { }
+
+    }
+}
